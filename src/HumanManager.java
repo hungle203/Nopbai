e94f2b5eb2092b7a246ef550ee8c7ef32603a899
hungle203 <leviethungkm2003@gmail.com>
2021-12-22 14:56:46 +0700

    ChuaXong

---
+++ src/HumanManager.java
@@ -0,0 +1,91 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class HumanManager {
+    private ArrayList<Human> humans;
+    private Scanner scanner = new Scanner(System.in);
+
+    public HumanManager() {
+        this.humans = new ArrayList<>();
+    }
+
+    public void displayAllHuman() {
+        for (Human h : humans) {
+            System.out.println(h);
+        }
+    }
+
+    public void addHuman(Human human) {
+        humans.add(human);
+    }
+
+    public Human humanUpdate(String name) {
+        Human humanUpdate = null;
+        for (Human human : humans) {
+            if (human.getName() == name) {
+                humanUpdate = human;
+                break;
+            }
+        }
+        if (humanUpdate != null) {
+            System.out.println("Nhập tên mới: ");
+            String n = scanner.nextLine();
+            humanUpdate.setName(n);
+            System.out.println("Nhập tuổi mới: ");
+            int age = scanner.nextInt();
+            humanUpdate.setAge(age);
+            System.out.println("Nhập giới tính mới: ");
+            String gen = scanner.nextLine();
+            humanUpdate.setGender(gen);
+            return humanUpdate;
+        }
+        return null;
+    }
+
+    public ArrayList<Human> displayByName(String name) {
+        ArrayList<Human> human = new ArrayList<>();
+        for (Human h : humans) {
+            if (h.getName().equals(name)) {
+                human.add(h);
+            }
+        }
+        return human;
+
+    }
+
+    public ArrayList<Human> displayByGender(String gender) {
+        ArrayList<Human> human = new ArrayList<>();
+        for (Human h : humans) {
+            if (h.getGender().equals(gender)) {
+                human.add(h);
+            }
+        }
+        return human;
+    }
+
+    public ArrayList<Human> displayByAge() {
+
+        ArrayList<Human> human = new ArrayList<>();
+        for (Human h : humans) {
+            if (h.getAge() < 20) {
+                human.add(h);
+            }
+        }
+        return human;
+    }
+
+    public Human createHuman(Scanner scanner) {
+        scanner.nextLine();
+        System.out.println("Nhập vào tên: ");
+        String name = scanner.nextLine();
+        System.out.println("Nhập vào tuổi ");
+        int age = scanner.nextInt();
+        System.out.println("Nhập vào giới tính: ");
+        String gen = scanner.nextLine();
+        System.out.println("Nhập vào địa chỉ: ");
+        String add = scanner.nextLine();
+        return new Human(name, age, gen, add);
+
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,68 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        HumanManager humanManager = new HumanManager();
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+
+        do {
+            System.out.println("Menu");
+            System.out.println("1. Thêm một người");
+            System.out.println("2. Sửa một ngưởi theo tên");
+            System.out.println("3. Xóa một người theo tên");
+            System.out.println("4. Hiển thị tất cả người");
+            System.out.println("5. Hiển thị tất cả nam");
+            System.out.println("6. Hiển thị tất cả nữ");
+            System.out.println("7. Hiển thị tất cả người dưới 20 tuổi");
+            System.out.println("8. Hiển thị tất cả người theo tên nhập vào");
+            System.out.println("0. Exit");
+            System.out.println("Nhập lựa chọn");
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    Human human = humanManager.createHuman(scanner);
+                    System.out.println();
+                    humanManager.addHuman(human);
+                    break;
+                case 2:
+                    System.out.println("Nhập vào tên muốn sửa");
+                    String name = scanner.nextLine();
+                    System.out.println(humanManager.humanUpdate(name));
+                    break;
+                case 3:
+                    System.out.println("Nhập vào tên muốn xóa");
+                    String nameDelete = scanner.nextLine();
+                    System.out.println(humanManager.humanUpdate(nameDelete));
+                    break;
+                case 4:
+                    humanManager.displayAllHuman();
+                    break;
+                case 5:
+                    System.out.println("Nhập vào giới tính muốn hiển thị");
+                    String gender = scanner.nextLine();
+                    if (gender.equals("Nam")) {
+                }
+
+                    break;
+
+                case 6:
+                    System.out.println();
+
+                    break;
+                case 7:
+                    humanManager.displayByAge();
+                    break;
+                case 8:
+                    System.out.println("Nhập vào tên muốn hiển thị");
+                    String nameDisplay = scanner.nextLine();
+                    ArrayList<Human> humans = new ArrayList<>();
+                    System.out.println(humanManager.displayByName(nameDisplay));
+                    break;
+            }
+        }while (choice!=0);
+    }
+}
